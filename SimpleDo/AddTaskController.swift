f877bcdd5b15660b9e4f69d1ff6858ae690fa50c
Adrian Kasiczak <adrian.kasiczak@icloud.com>
2018-03-13 22:48:19 +0100

    Worked on adding tasks ViewController

--- SimpleDo/AddTaskController.swift
+++ SimpleDo/AddTaskController.swift
@@ -17,12 +17,16 @@ class AddTaskController: UIViewController {
 
         // Do any additional setup after loading the view.
         view.backgroundColor = emerald
+        setupViews()
+        setupContraints()
     }
     
     let addTaskLabel: UILabel = {
         let label = UILabel()
         label.text = "What do you want to do?"
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Helvetica", size: 17)
+        label.textColor = .black
         return label
     }()
     
@@ -30,14 +34,60 @@ class AddTaskController: UIViewController {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.text = "Type here..."
+        textView.font = UIFont(name: "Helvetica", size: 24)
+        textView.textColor = .white
+        textView.backgroundColor = .none
+        textView.isEditable = true
         return textView
     }()
     
-    func setupViews() {
+    let doneButton: UIButton = {
+       let btn = UIButton()
         
+        return btn
+    }()
+    
+    let cancelButton: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .white
+        btn.setTitle("Cancel", for: .normal)
+        btn.setTitleColor(UIColor(red: 80/255, green: 200/255, blue: 120/255, alpha: 1), for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(cancelButtonAction), for: .touchUpInside)
+        return btn
+    }()
+    
+    
+    @objc func cancelButtonAction() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupViews() {
+        view.addSubview(addTaskLabel)
+        view.addSubview(addTaskTextView)
+        view.addSubview(doneButton)
+        view.addSubview(cancelButton)
     }
     
     func setupContraints() {
+        addTaskLabel.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        addTaskLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        addTaskLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 25).isActive = true
+        addTaskLabel.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 75).isActive = true
         
+        addTaskTextView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        addTaskTextView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        addTaskTextView.topAnchor.constraint(equalTo: addTaskLabel.bottomAnchor, constant: 10).isActive = true
+        addTaskTextView.centerXAnchor.constraint(equalTo: addTaskLabel.centerXAnchor).isActive = true
+        
+//        doneButton.widthAnchor
+//        doneButton.widthAnchor
+//        doneButton.widthAnchor
+//        doneButton.widthAnchor
+        
+        cancelButton.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        cancelButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
--- SimpleDo/TaskViewController.swift
+++ SimpleDo/TaskViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class TaskViewController: UITableViewController {
 
+    let cellID = "cellID"
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,10 +19,22 @@ class TaskViewController: UITableViewController {
         view.backgroundColor = .white
         navigationItem.title = "Inbox"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTaskController))
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
     
     @objc func addTaskController() {
         present(AddTaskController(), animated: true, completion: nil)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        return cell
     }
     
 }
